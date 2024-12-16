fb922e012370e8869e0bad0ec560d4b5f90e8ddd
mark-drastrup <mark.drastrup@gmail.com>
2024-12-16 22:11:46 +0100

    feat: audio player

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import { Hero } from "@/components/Hero/Hero";
+import { SongList } from "@/components/SongList/SongList";
 
 export default function LandingPage() {
   return (
     <main className={styles["main-content"]}>
       <Hero />
+      <SongList />
     </main>
   );
 }
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,3 +2,6 @@ export * from "./BurgerMenu";
 export * from "./LogoAndNameWhite";
 export * from "./LogoWhite";
 export * from "./Waves";
+export * from "./SpeakerLoud";
+export * from "./Play";
+export * from "./Pause";
