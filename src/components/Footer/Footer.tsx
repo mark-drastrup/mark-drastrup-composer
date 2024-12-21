b4136f8eadf4336de46c5fe752913f1640ab917f
mark-drastrup <mark.drastrup@gmail.com>
2024-12-21 23:33:33 +0100

    feat: footer layout

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,27 +4,30 @@ import styles from "./footer.module.css";
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <LogoPurple className={styles.logo} />
+      <div className={styles.wrapper}>
+        <LogoPurple className={styles.logo} />
 
-      <nav>
-        <ul className={styles["nav-list"]}>
-          <li className={styles["list-item"]}>
-            <a href="/#home">Home</a>
-          </li>
-          <li className={styles["list-item"]}>
-            <a href="/#songs">Songs</a>
-          </li>
-          <li className={styles["list-item"]}>
-            <a href="/#about">About</a>
-          </li>
-          <li className={styles["list-item"]}>
-            <a href="/#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
+        <nav>
+          <ul className={styles["nav-list"]}>
+            <li className={styles["list-item"]}>
+              <a href="/#home">Home</a>
+            </li>
+            <li className={styles["list-item"]}>
+              <a href="/#songs">Songs</a>
+            </li>
+            <li className={styles["list-item"]}>
+              <a href="/#about">About</a>
+            </li>
+            <li className={styles["list-item"]}>
+              <a href="/#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
 
       <p className={styles.copyright}>
-        © 2024 your awesome website. <br /> All rights reserved.
+        © 2024 your awesome website. <br className={styles.break} /> All rights
+        reserved.
       </p>
     </footer>
   );
