99e5ed5b8b122540eff05cbc57d83e47499110c9
mark-drastrup <mark.drastrup@gmail.com>
2025-01-10 23:24:56 +0900

    feat: about me section layout

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,8 +4,12 @@ import styles from "./hero.module.css";
 export function Hero() {
   return (
     <section className={styles["hero-wrapper"]}>
-      <h1 className={styles.title}>
+      {/* <h1 className={styles.title}>
         COMPOSER FOR GAMES <br /> AND FILM
+      </h1> */}
+
+      <h1 className={styles.title}>
+        VIDEO GAME MUSIC & <br /> FILM COMPOSER
       </h1>
 
       <iframe
