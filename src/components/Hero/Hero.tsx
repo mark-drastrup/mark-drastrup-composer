36aa84636ad83ac7fb5519b45e1c5fc9681a3b36
mark-drastrup <mark.drastrup@gmail.com>
2026-01-19 17:49:11 +0900

    fix: change embedded youtube video

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,22 +4,27 @@ import styles from "./hero.module.css";
 export function Hero() {
   return (
     <section className={styles["hero-wrapper"]}>
-      {/* <h1 className={styles.title}>
-        COMPOSER FOR GAMES <br /> AND FILM
-      </h1> */}
-
       <h1 className={styles.title}>
         VIDEO GAME MUSIC & <br /> FILM COMPOSER
       </h1>
 
       <iframe
         className={styles.video}
-        src="https://www.youtube.com/embed/K3HzzcI1cxE?si=ythiWE5rdNDW86ak"
+        src="https://www.youtube.com/embed/sXQfRxrsMMY?si=OfJOrh2Gc6_l_NF4"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
       ></iframe>
+
+      {/* <iframe
+        className={styles.video}
+        src="https://www.youtube.com/embed/eYcRfxX18XI?si=kgsQBqw-xWC1ziai"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe> */}
       <Waves className={styles.waves} />
     </section>
   );
