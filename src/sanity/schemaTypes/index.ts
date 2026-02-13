5e1d0830e681f4843d1c51ca6c89534d966c273d
mark-drastrup <mark.drastrup@gmail.com>
2026-02-14 01:17:57 +0900

    feat: add schema types for category, course, lesson, module, and update index

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,9 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import { courseType } from "./courseType";
+import { lessonType } from "./lessonType";
+import { categoryType } from "./categoryType";
+import { moduleType } from "./moduleType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [courseType, lessonType, categoryType, moduleType],
+};
